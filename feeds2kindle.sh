d6ae997fabea13261242ce9063b48fd16c671754
mahnkong <mahnkong@gmx.de>
2017-04-29 18:28:06 +0000

    wip

--- feeds2kindle.sh
+++ feeds2kindle.sh
@@ -24,7 +24,7 @@ if [ "$CALIBRE_FEED_RECIPE" = "" ]; then
     echo "CALIBRE_FEED_RECIPE not defined!"
     exit 1
 fi
-echo "converted feed '${CALIBRE_FEED_RECIPE}'..."
+echo "converting feed '${CALIBRE_FEED_RECIPE}'..."
 ebook-convert "${CALIBRE_FEED_RECIPE}".recipe feed.mobi
 echo "sending converted feed '${CALIBRE_FEED_RECIPE}' to '${KINDLE_EMAIL_ADDR}'"
 calibre-smtp -r $SMTP_RELAY --username $SMTP_USERNAME --password $SMTP_PASSWORD --port $SMTP_PORT $SMTP_USERNAME $KINDLE_EMAIL_ADDR '' -s "${CALIBRE_FEED_RECIPE}" -a feed.mobi
